9f87af6ae85b879cf1ddebb30ce354e567b29c30
Janco <Steenkamp>
2020-09-18 12:50:35 +0200

    CREATED MPHO'S STORED PROCS

---
+++ SQL DB/SP_RESERVATIONS_BY_STATUS.sql
@@ -0,0 +1,20 @@
+CREATE PROCEDURE SP_RESERVATIONS_BY_STATUS @RESERVATION_STATUS_ID INT
+AS
+SELECT 
+	R1.Reservation_Id
+	,R1.User_Id_FK
+	,R1.Reservation_date_Created
+	,R1.Reservation_Date_Reserved
+	,R1.Reservation_NumberOfBills
+	,R1.Reservation_Party_Qty
+	,RS.Reservation_Status
+	,R2.Restaurant_Name
+	,R2.Restaurant_Address_Line_1
+	,R2.Resaturant_Address_Line_2
+	,R2.Restaurant_City
+FROM [dbo].[Reservation] R1
+LEFT JOIN [dbo].[Reservation_Status] RS ON RS.Reservation_Status_Id = R1.Reservation_Status_Id_FK
+LEFT JOIN [dbo].[Reservation_Restaurant] RR ON RR.Reservation_Id_FK = R1.Reservation_Id
+LEFT JOIN [dbo].[Restaurant] R2 ON R2.Restaurant_Id = RR.Restaurant_Id_FK
+
+WHERE R1.Reservation_Status_Id_FK = @RESERVATION_STATUS_ID
