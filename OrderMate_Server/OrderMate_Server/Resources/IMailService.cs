420ac6324dd5cd7f5a04a0ba091337e616ca4256
Mpho <mphonokomosotho@gmail.com>
2020-09-25 17:14:47 +0200

    Back end

---
+++ OrderMate_Server/OrderMate_Server/Resources/IMailService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MimeKit;
+using OrderMate_Server.Resources;
+
+namespace Contacts.Interfaces
+{
+    public interface IMailService
+    {
+        Task SendEmailAsync(MailRequest mailRequest);
+    }
+}
---
+++ OrderMate_Server/OrderMate_Server/Resources/MailRequest.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderMate_Server.Resources
+{
+    public class MailRequest
+    {
+        public string ToEmail { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+        public List<IFormFile> Attatchments { get; set; }
+    }
+}
